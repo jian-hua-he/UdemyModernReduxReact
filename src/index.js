53df7141e12e5f35749cf65f9f1a69a58c6e82f1
Jian-Hua, He <game09116@gmail.com>
2016-12-17 23:27:04 +0800

    Add google API key

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyByk2qFYYfNilUq04kKJ8uU6m98ZqeJWAY';
+
 const App = () => <div>Hi</div>;
 
 ReactDOM.render(<App />, document.querySelector('.container'));

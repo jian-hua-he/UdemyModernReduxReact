3cd8a76e1c47871ccddadf5e7b4453d39e0dfe63
Jian-Hua, He <game09116@gmail.com>
2016-12-26 19:27:00 +0800

    Fetch Youtube data and use class syntax to substitute function syntax

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyByk2qFYYfNilUq04kKJ8uU6m98ZqeJWAY';
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
-};
+YTSearch({
+    key: API_KEY,
+    term: 'surfboards',
+}, function (data) {
+    console.log(data);
+});
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));

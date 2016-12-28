2200d4739d708fdcb820c503ebb655d2744cabe0
Jian-Hua, He <game09116@gmail.com>
2016-12-28 12:48:33 +0800

    Fix syntax error

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -7,12 +7,12 @@ const VideoDetail = (props) => {
 
     const video = props.video;
     const videoId = video.id.videoId;
-    const url = `https:://www.youtube.com/embed/${videoId}`;
+    const url = `https://www.youtube.com/embed/${videoId}`;
 
     return (
         <div className="video-detail col-md-8">
             <div className="embed-responsive embed-responsive-16by9">
-                <iframe class="embed-responsive-item" src={ url }></iframe>
+                <iframe className="embed-responsive-item" src={ url }></iframe>
             </div>
             <div className="details">
                 <div>{ video.snippet.title }</div>

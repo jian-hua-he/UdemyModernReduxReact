d3220cc4a0d5e81741906ddc4f17de564d4e04ab
Jian-Hua, He <game09116@gmail.com>
2017-01-13 15:37:09 +0800

    Add promise to fetchWeather function and attach to payload attribute

--- WeatherExample/src/actions/index.js
+++ WeatherExample/src/actions/index.js
@@ -1,14 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = '3544e9a0116381634e94077a57c0c860';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?mode=json&appid=${API_KEY}`;
-const FETCH_WEATHER = 'FETCH_WEATHER';
 
-function fetchWeather(city) {
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
     let url = `${ROOT_URL}&q=${city},us`;
+    let request = axios.get(url);
 
     return {
         type: FETCH_WEATHER,
+        payload: request,
     };
 }
-
-export FETCH_WEATHER;
-export fetchWeather;

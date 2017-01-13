f53e8e6d590f0fb736ca8920580c7abd09bc76ef
Jian-Hua, He <game09116@gmail.com>
2017-01-13 13:45:07 +0800

    Implement index action creator

--- WeatherExample/src/actions/index.js
+++ WeatherExample/src/actions/index.js
@@ -1,1 +1,14 @@
 const API_KEY = '3544e9a0116381634e94077a57c0c860';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?mode=json&appid=${API_KEY}`;
+const FETCH_WEATHER = 'FETCH_WEATHER';
+
+function fetchWeather(city) {
+    let url = `${ROOT_URL}&q=${city},us`;
+
+    return {
+        type: FETCH_WEATHER,
+    };
+}
+
+export FETCH_WEATHER;
+export fetchWeather;

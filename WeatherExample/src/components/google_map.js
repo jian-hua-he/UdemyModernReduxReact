cb6698a12bd1012935629879e01a7a971fe5d2de
Jian-Hua, He <game09116@gmail.com>
2017-01-17 23:35:57 +0800

    Fix syntax error

--- WeatherExample/src/components/google_map.js
+++ WeatherExample/src/components/google_map.js
@@ -4,9 +4,9 @@ import { GoogleMapLoader, GoogleMap } from 'react-google-maps';
 export default (props) => {
     return (
         <GoogleMapLoader
-            containerElement={ <div style={ height: '100%' }></div> }
+            containerElement={ <div style={{ height: '100%' }} /> }
             googleMapElement={
-                <GoogleMap defaultZoom={ 12 } defaultCenter={ lat: props.lat, lng: props.lon } />
+                <GoogleMap defaultZoom={ 12 } defaultCenter={{ lat: props.lat, lng: props.lon }} />
             } />
     );
 }

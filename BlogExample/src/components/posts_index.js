9186e9caab6f9e4c239b21d5486f2f70da466f47
Jian-Hua, He <game09116@gmail.com>
2017-01-25 19:29:37 +0800

    Convert PostsIndex components to container

--- BlogExample/src/components/posts_index.js
+++ BlogExample/src/components/posts_index.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchPosts } from '../actions/index';
 
 class PostsIndex extends Component {
     componentWillMount() {
-        console.log('this would be a good time to call action creator to fetch posts');
+        this.props.fetchPosts();
     }
 
     render() {
@@ -12,4 +15,8 @@ class PostsIndex extends Component {
     }
 }
 
-export default PostsIndex;
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ fetchPosts }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(PostsIndex);

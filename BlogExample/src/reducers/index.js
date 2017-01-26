cdf7b3a7d80c37ce174207bd987ff717b727073c
Jian-Hua, He <game09116@gmail.com>
2017-01-26 12:59:33 +0800

    Add form reducer

--- BlogExample/src/reducers/index.js
+++ BlogExample/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import PostsRecuder from './reducer_posts';
+import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
     post: PostsRecuder,
+    form: formReducer,
 });
 
 export default rootReducer;

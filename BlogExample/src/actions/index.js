4e37c52dc4469baec54a29cadbed5ecb8c3ec9b9
Jian-Hua, He <game09116@gmail.com>
2017-01-28 20:57:02 +0800

    Add delete action

--- BlogExample/src/actions/index.js
+++ BlogExample/src/actions/index.js
@@ -6,6 +6,7 @@ const API_KEY = '?key=jianhuahe';
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
 export const FETCH_POST = 'FETCH_POST';
+export const DELETE_POST = 'DELETE_POST';
 
 export function fetchPosts() {
     const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
@@ -22,7 +23,7 @@ export function createPost(props) {
     return {
         type: CREATE_POST,
         payload: request,
-    }
+    };
 }
 
 export function fetchPost(id) {
@@ -31,5 +32,14 @@ export function fetchPost(id) {
     return {
         type: FETCH_POST,
         payload: request,
-    }
+    };
+}
+
+export function deletePost(id) {
+    const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+    return {
+        type: DELETE_POST,
+        payload: request,
+    };
 }
--- BlogExample/src/components/posts_show.js
+++ BlogExample/src/components/posts_show.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchPost } from '../actions/index';
+import { Link } from 'react-router';
+import { fetchPost, deletePost } from '../actions/index';
 
 class PostsShow extends Component
 {
@@ -8,11 +9,35 @@ class PostsShow extends Component
         this.props.fetchPost(this.props.params.id);
     }
 
+    onDeleteClick() {
+        this.props.deletePost(this.props.params.id);
+    }
+
     render() {
+        const { post } = this.props;
+
+        if ( ! post) {
+            return (
+                <div>loading</div>
+            );
+        }
+
         return (
-            <div>Show post { this.props.params.id }</div>
+            <div>
+                <Link to="/">Back To Index</Link>
+                <button className="btn btn-danger pull-xs-right" onClick={ this.onDeleteClick.bind(this) }>Delete Post</button>
+                <h3>{ post.title }</h3>
+                <h6>Categories: { post.categories }</h6>
+                <p>{ post.content }</p>
+            </div>
         );
     }
 }
 
-export default connect(null, { fetchPost })(PostsShow);
+function mapStateToProps(state) {
+    return {
+        post: state.post.post,
+    };
+}
+
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
